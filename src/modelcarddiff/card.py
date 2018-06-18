8ddf27bfb2d70443cc2940453754ddd39b65450e
Xiaoxiao Cui <34017421+rabbittsui@users.noreply.github.com>
2018-06-18 11:20:00 +0800

    docs: document governance

---
+++ src/modelcarddiff/card.py
@@ -0,0 +1,22 @@
+"""Parse the model card format into sections, claims, and results.
+
+Card format
+-----------
+
+A card is Markdown with a small set of conventions:
+
+- ``## Section Name`` starts a section. Everything until the next ``##`` heading
+  is the body of that section. A leading ``# Title`` line is treated as the card
+  title, not a section.
+- A capability claim is a bullet in the Capabilities section written as::
+
+      - claim: <text> (cites: <eval-id>)
+
+  The ``(cites: <eval-id>)`` suffix is optional. A claim without it is an
+  uncited claim, recorded with ``cites`` set to ``None``.
+- A limitation is any bullet in the Limitations section.
+- A result is a line in the Results section written as::
+
+      <eval-id> = <metric-name> <value>
+
+  The value is parsed as a float when possible, otherwise kept as text.
