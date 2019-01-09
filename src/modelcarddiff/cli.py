ab3ee563ed564a4e1cc27589a76c160855b08c40
Xiaoxiao Cui <34017421+rabbittsui@users.noreply.github.com>
2019-01-09 17:28:00 +0800

    test: cover audit

---
+++ src/modelcarddiff/cli.py
@@ -0,0 +1,11 @@
+"""Command line interface for modelcarddiff.
+
+Subcommands:
+
+    gate     check a card against a schema and pass or refuse
+    diff     compare two card versions and classify each change
+    claims   show which evaluation each capability claim cites
+    version  print the package version
+
+Exit codes: 0 clean, 1 findings present (gate refused or a regression found),
+2 usage error. argparse itself exits with 2 on argument errors, which matches
