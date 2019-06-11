f7a3ee667ef36b9c7c832b9aec95d64d3b3ef76b
Xiaoxiao Cui <34017421+rabbittsui@users.noreply.github.com>
2019-06-11 10:17:00 +0800

    docs: document reports

--- tests/test_card.py
+++ tests/test_card.py
@@ -9,3 +9,8 @@ SAMPLES = os.path.join(os.path.dirname(__file__), "..", "samples")
 
 
 class ParseTests(unittest.TestCase):
+    def test_title_and_sections(self):
+        text = "# Model Card: x\n\n## Overview\n\nbody\n\n## Results\n\nid = m 1\n"
+        parsed = card.parse(text)
+        self.assertEqual(parsed.title, "Model Card: x")
+        self.assertIsNotNone(parsed.section("Overview"))
