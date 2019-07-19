d114ec3d79b706cf33233ef00dda7801e2cbcdd7
Xiaoxiao Cui <34017421+rabbittsui@users.noreply.github.com>
2019-07-19 16:15:00 +0800

    feat: diff

--- tests/test_claims.py
+++ tests/test_claims.py
@@ -6,3 +6,10 @@ import unittest
 from modelcarddiff import card, claims
 
 SAMPLES = os.path.join(os.path.dirname(__file__), "..", "samples")
+
+
+class CitationTests(unittest.TestCase):
+    def test_resolved_citation(self):
+        parsed = card.parse(
+            "## Capabilities\n- claim: t (cites: e)\n## Results\ne = m 1\n"
+        )
