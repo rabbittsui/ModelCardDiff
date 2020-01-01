35949f702696354e9d3972f918657674743a686b
Xiaoxiao Cui <34017421+rabbittsui@users.noreply.github.com>
2020-01-01 14:28:00 +0800

    docs: document model-cards

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -4,3 +4,9 @@ import io
 import os
 import unittest
 from contextlib import redirect_stdout
+
+from modelcarddiff import cli
+
+SAMPLES = os.path.join(os.path.dirname(__file__), "..", "samples")
+
+
