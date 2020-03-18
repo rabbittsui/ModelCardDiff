8b5c33705e32fef446e89995bb3ef59c15e2893a
Xiaoxiao Cui <34017421+rabbittsui@users.noreply.github.com>
2020-03-18 11:20:00 +0800

    feat: add compliance

--- tests/test_gate.py
+++ tests/test_gate.py
@@ -4,3 +4,9 @@ import os
 import unittest
 
 from modelcarddiff import card, gate, schema
+
+SAMPLES = os.path.join(os.path.dirname(__file__), "..", "samples")
+
+
+def _load(name):
+    return card.parse_file(os.path.join(SAMPLES, name))
