3e7cb4792bde782aef23a2fbf47c08a7b51f8b9e
Xiaoxiao Cui <34017421+rabbittsui@users.noreply.github.com>
2020-03-31 10:00:00 +0800

    docs: document reports

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -7,3 +7,8 @@ from modelcarddiff import card, schema
 
 SAMPLES = os.path.join(os.path.dirname(__file__), "..", "samples")
 
+
+class SchemaTests(unittest.TestCase):
+    def test_parse_requires(self):
+        parsed = schema.parse("require Overview\nrequire Results\n")
+        self.assertEqual(parsed.required_sections, ("Overview", "Results"))
